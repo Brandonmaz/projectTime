954d7639968815090e0d5561c29c54e23c6f21cc
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-01-04 16:18:23 -0800

    added a searchable API

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const _ = require('lodash')
 const Product = require('../models/products.js')
 const tableTop = require('../models/tableTop.js')
 const tableTopia = require('../models/tableTopia.js')
+const product = require('../data/product.js')
 const products = express.Router()
 
 const isAuthenticated = (req, res, next) => {
@@ -11,7 +13,42 @@ const isAuthenticated = (req, res, next) => {
     res.redirect('/sessions/new')
   }
 }
-// testing
+//=============Searchable API==================
+// https://jonathanmh.com/building-a-simple-searchable-api-with-express-backend/
+products.get('/api', (req, res) => {
+  var response = []
+  if(typeof req.query.name != 'undefined'){
+    product.filter(function(product){
+      if(product.name.toLowerCase() === req.query.name.toLowerCase()){
+        response.push(product)
+      }
+    })
+  }
+  response = _.uniqBy(response, 'id')
+
+  if(Object.keys(req.query).length === 0){
+    response = product
+  }
+  res.json(response)
+})
+// ===================== Search Bar jQuery =================
+// $(() => {
+//     const formTest =
+//       $('form').on('submit', (event) => {
+//         event.preventDefault()
+//         const userInput = $('input').val()
+//       })
+//     $.ajax({
+//       url: `https://products/api/name=` + userInput + ``,
+//       type: "GET"
+//     }).then(
+//       (data) => {
+//       }, (error) => {
+//       alert(`Please check your spelling and try again`)
+//       console.log(`${error.statusText.toUpperCase()}:bad request`);
+//     }
+//   )
+// })
 
 
 //============================Main==============================
--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   username: {type: String, unique: true, required: true},
-  password: String
+  password: String,
 })
 const User = mongoose.model('User', userSchema)
 module.exports = User
