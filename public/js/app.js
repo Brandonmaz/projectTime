f7117ebc02a0337798d6e4883fb6498794b1d7c4
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-01-05 14:55:22 -0800

    test

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 // console.log('hello');
 $(() => {
     const formTest =
-      $('form[role=search]').on('submit', (event) => {
+      $('form[role=search] input').on('submit', (event) => {
         event.preventDefault()
         const userInput = $('input').val()
         console.log(userInput);
