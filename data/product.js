cacef81926e669b7b7c91eb5d0795708c2925826
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-01-07 20:36:05 -0800

    changes made

--- data/product.js
+++ data/product.js
@@ -10,7 +10,7 @@ const product = [
     players: '3 to 5',
     description: `In Dead of Winter, players are a colony of survivors trying to survive a hazardous winter in the aftermath of the zombie apocalypse. The colony has an objective they are trying to complete, but each player also controls a group of survivors that has a secret objective. Often this secret objective requires that a player help complete the colony objective. Only players who have completed their secret objective when the game ends will win. The game can end in a variety of ways: If the morale track reaches 0, if the round track reaches 0, or if the main objective is completed.`,
     price: 1.99,
-    qty: 1
+    qty: 100
   },
   {
     id: 2,
@@ -23,7 +23,7 @@ const product = [
     players: '2-5',
     description: `Isle of Skye is one of the most beautiful places in the world, with soft sand beaches, gently sloping hills, and impressive mountains. The landscape of Isle of Skye is breathtaking and fascinates everyone. In the tile-laying game Isle of Skye: From Chieftain to King, 2-5 players are chieftains of famous clans and want to build their kingdoms to score as many points as possible — but in each game only four of the sixteen scoring tiles will be scored. Thanks to the scoring tiles, each game is different and leads to different tactics and strategies, but having enough money is useful no matter what else is going on. Managing that money can be tricky, though. Each turn, each player places two area tiles in front of them and sets the selling price for the tiles. Setting a high price is great, but only so long as someone actually pays the price because if no one opts to buy, then the seller must buy the tiles at the price they previously requested. In the end, the player with the best kingdom — and not the richest player — becomes the sovereign of the island.`,
     price: 3.99,
-    qty: 2
+    qty: 100
   },
   {
     id: 3,
@@ -37,7 +37,7 @@ const product = [
     description: `Burgle your way to adventure in the deck-building board game Clank! Sneak into an angry dragon's mountain lair to steal precious artifacts. Delve deeper to find more valuable loot. Acquire cards for your deck and watch your thievish abilities grow.
     Be quick and be quiet. One false step and CLANK! Each careless sound draws the attention of the dragon, and each artifact stolen increases its rage. You can enjoy your plunder only if you make it out of the depths alive!`,
     price: 5.99,
-    qty: 3
+    qty: 100
   },
   {
     id: 4,
@@ -50,7 +50,7 @@ const product = [
     players: '2-4',
     description: `Be the King of the board in this fast paced action filled game. Push your limits to how long you can be king of the hill, but be careful, everybody will be trying to push you off that hill!`,
     price: 7.99,
-    qty: 4
+    qty: 100
   },
   {
     id: 5,
@@ -63,7 +63,7 @@ const product = [
     players: '2-4',
     description: `Burgle Bros is a great cooperative "heist" game for 1-4 players. Use your team to explore the office building, avoid the patrolling guards, find the loot, and escape before you get caught!`,
     price: 0,
-    qty: 5
+    qty: 100
   },
   {
     id: 6,
@@ -76,7 +76,7 @@ const product = [
     players: '2-5',
     description: `It is a time of unrest in 1920s Europa. The ashes from the first great war still darken the snow. The capitalistic city-state known simply as “The Factory,” which fueled the war with heavily armored mechs, has closed its doors, drawing the attention of several nearby countries. In Scythe, each player represents a fallen leader attempting to restore their honor and lead their faction to power in Eastern Europa. Players conquer territory, enlist new recruits, reap resources, gain villagers, build structures, and activate monstrous mechs. Every part of Scythe has an aspect of engine-building to it. Players can upgrade actions to become more efficient, build structures that improve their position on the map, enlist new recruits to enhance character abilities, activate mechs to deter opponents from invading, and expand their borders to reap greater types and quantities of resources. These engine-building aspects create a sense of momentum and progress throughout the game. The order in which players improve their engine adds to the unique feel of each game, even when playing one faction multiple times.`,
     price: 7.99,
-    qty: 6
+    qty: 100
   },
   {
     id: 7,
@@ -89,7 +89,7 @@ const product = [
     players: '2-4',
     description: `The postcards in every local drug store read, "Welcome to Fabulous Farmingdale!", an ad campaign that was the brainchild of Mayor Hernandez (who coincidentally employed his wife's public relations firm to market their community). But right now, things are far from fabulous in Farmingdale and, for once, everyone isn't blaming the Mayor. Some kind of virus or poison is turning ordinary people into vicious, zombie-like killers. It is not clear how the disease spreads (though it seems that physical contact is certainly one way), but it is obvious what the illness does to its victims. These undead, nicknamed "Zeds" from the local newscasts as the acronym for "Zombie Epidemic Disease," are now converging on your corner of the world around Farmingdale. As best you can tell, you have been left to your own devices to stop them while the National Guard organizes a relief column, but that could take days, perhaps weeks, for them to fight their way to you and until then, what can you do? With little choice between survival and a gruesome (un)death, you realize that you must coordinate the defense of the town of Farmingdale and its surrounding villages. You must lead the good citizens and emerging heroes of these communities to halt the Zeds' advances by (re)killing them, attempt to coordinate the discovery of a cure to this vile scourge, and preserve as much of the area and as many of its inhabitants as possible. There's no time to lose...`,
     price: 7.99,
-    qty: 7
+    qty: 100
   },
   {id: 8,
     name: 'Gloomhaven',
@@ -101,7 +101,7 @@ const product = [
     players: '2',
     description: `If you are looking for an upgraded version of D&D, look no further. Gloomhaven will have you and your friends playing for hours, strategically planning out your next move. Remember that this is a game of whyts and working as a team. Set aside a couple hours out your day and enjoy this awesome game!`,
     price: 0,
-    qty: 8
+    qty: 100
   },
   {
     id: 9,
@@ -114,7 +114,7 @@ const product = [
     players: '2-4',
     description: `Introduced by the Moors, azulejos (originally white and blue ceramic tiles) were fully embraced by the Portuguese when their king Manuel I, on a visit to the Alhambra palace in Southern Spain, was mesmerized by the stunning beauty of the Moorish decorative tiles. The king, awestruck by the interior beauty of the Alhambra, immediately ordered that his own palace in Portugal be decorated with similar wall tiles. As a tile-laying artist, you have been challenged to embellish the walls of the Royal Palace of Evora. In the game Azul, players take turns drafting colored tiles from suppliers to their player board. Later in the round, players score points based on how they've placed their tiles to decorate the palace. Extra points are scored for specific patterns and completing sets; wasted supplies harm the player's score. The player with the most points at the end of the game wins.`,
     price: 1.99,
-    qty: 9
+    qty: 100
   },
   {
     id: 10,
@@ -127,7 +127,7 @@ const product = [
     players: '2-4',
     description: `Revolt! Yes, my dear fellow animals, revolt against the humans who exploit us! I am a lab rat who broke free, and I will have my vengeance over those humans! Join me and together we shall blow up this place, the symbol of our oppression! In ARGH, a minimalist game with bluff and deduction, your mission is to find allies and the components to build a bomb. Avoid the humans and the animals that remain faithful to them. Make smart deductions and careful propositions, and thanks to you ARGH will become a landmark in the history of the struggle for animal liberation.`,
     price: 2.99,
-    qty: 10
+    qty: 100
   },
   {
     id: 11,
@@ -140,7 +140,7 @@ const product = [
     players: '1-4',
     description: `After a thousand years of peace, Dragul armies raided from the north, prompting you and every other loyal citizen of Nalos to sign up as soldiers of the King’s Guard. The armies of Nalos fought long and hard against the North’s monstrous generals and their minions, finally turning the tide of the Dragul Invasion. But new threats arise to jeopardize all of Ulos. A mysterious cult prepares for the return of an ancient god. The long-abandoned gate at Brizoor Waal, a portal between planes, rumbles with new life. Strange rifts appear in the sky, unleashing bizarre creatures into your world. Dragul invaders seek to exploit this instability to conquer Nalos. As a humble soldier of the King’s Guard, you are called away from your regiment on a special mission that will allow you to uncover the secrets of your world. You will discover powerful artifacts and meet members of the cult working to unearth them. You will confront Dragul warriors and discover the motives behind their invasion. And you will encounter strange beings from other planes who threaten to disrupt the very fabric of your universe.`,
     price: 3.99,
-    qty: 1
+    qty: 100
   },
   {
     id: 12,
@@ -153,7 +153,7 @@ const product = [
     players: '2-5',
     description: `Word-building meets deck-building in the unique game Paperback. Players start with a deck of letter cards and wild cards. Each hand they form words, and purchase more powerful letters based on how well their word scored. Most letters have abilities that activate when they are used in a word, such as drawing more cards or double letter score. Players buy wilds to gain victory points. Variant included for cooperative play.`,
     price: 4.99,
-    qty: 2
+    qty: 100
   },
   {
     id: 13,
@@ -166,7 +166,7 @@ const product = [
     players: '2-4',
     description: `Santorini is a non-abstract re-imagining of the 2004 edition. Since its original inception over 30 years ago, Santorini has been endlessly developed, enhanced and refined by mathematician and educator, Dr. Gordon Hamilton. Santorini is a highly accessible pure strategy game that is simple enough for an elementary school classroom. But with enough gameplay depth and content for even hardcore gamers to explore, Santorini is truly a game for everyone.`,
     price: 1.99,
-    qty: 3
+    qty: 100
   },
   {
     id: 14,
@@ -179,7 +179,7 @@ const product = [
     players: '1-6',
     description: `Burgle Bros is a great cooperative "heist" game for 1-4 players. Use your team to explore the office building, avoid the patrolling guards, find the loot, and escape before you get caught!`,
     price: 7.99,
-    qty: 4
+    qty: 100
   },
   {
     id: 15,
@@ -192,7 +192,7 @@ const product = [
     players: '5-8',
     description: `The year is 1932. The place is pre-WWII Germany. In Secret Hitler, players are German politicians attempting to hold a fragile Liberal government together and stem the rising tide of Fascism. Watch out though — there are secret Fascists among you, and one player is Secret Hitler.`,
     price: 0,
-    qty: 5
+    qty: 100
   },
   {
     id: 16,
@@ -205,7 +205,7 @@ const product = [
     players: '1-6',
     description: `In the multiplayer puzzle game Railroad Ink, your goal is to connect as many exits on your board as possible. Each round, a set of dice are rolled in the middle of the table, determining which kind of road and railway routes are available to all players. You have to draw these routes on your erasable boards to create transport lines and connect your exits, trying to optimize the available symbols better than your opponents. The more exits you connect, the more points you score at the end of the game, but you lose points for each incomplete route, so plan carefully! Will you press your luck and try to stretch your transportation network to the next exit, or will you play it safe and start a new, simpler to manage route?`,
     price: 6.99,
-    qty: 6
+    qty: 100
   },
   {
     id: 17,
@@ -218,7 +218,7 @@ const product = [
     players: '1-5',
     description: `K2 is the second-highest mountain on Earth as well as the second deadliest. It's known as the Savage Mountain, as it kills one person for every four who have reached the summit... Now your team stands in its shadow, ready to climb for fame and glory. You know the dangers of K2 all too well. Extreme weather with frequent and deadly storms, exposed, steep, tricky routes and lack of oxygen await you on your way to the summit. You will test your climbing skills to death, try to outsmart the ever-changing weather and always worry about the acclimatization of your 2 mountaineers. Also there are the other teams ready to take the glory for themselves`,
     price: 2.99,
-    qty: 7
+    qty: 100
   },
   {
     id: 18,
@@ -231,7 +231,7 @@ const product = [
     players: '2-5',
     description: `Stockpile is an economic board game that combines the traditional stockholding strategy of buy low, sell high with several additional mechanisms to create a fast-paced, engaging and interactive experience. In Stockpile, players act as stock market investors at the end of the 20th century hoping to strike it rich, and the investor with the most money at the end of the game is the winner. Stockpile centers around the idea that nobody knows everything about the stock market, but everyone does know something. In the game, this philosophy manifests in two ways: insider information and the stockpile.`,
     price: 4.99,
-    qty: 8
+    qty: 100
   }
 ]
 module.exports = product
